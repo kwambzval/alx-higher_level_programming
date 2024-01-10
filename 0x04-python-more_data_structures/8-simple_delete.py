616ca23a6bd75661031749d086599497593fa130
kwambzval <valentinevkkv@gmail.com>
2024-01-10 11:40:06 -0800

    task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    """Returns a key with the biggest integer value."""
-    if a_dictionary:
-        return max(a_dictionary, key=a_dictionary.get)
-    return None
+def simple_delete(a_dictionary, key=""):
+    """Deletes a key in a dictionary."""
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
