b645e2039368bcfdb4f3baee581dd302146f85a8
kwambzval <valentinevkkv@gmail.com>
2024-01-23 14:06:12 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    This is a class that defines a square.
+    """
+    def __init__(self, size):
+        """
+        Initialize Square instance with size.
+
+        Args:
+            size: The size of the sides of the Square.
+        """
+        self.__size = size
