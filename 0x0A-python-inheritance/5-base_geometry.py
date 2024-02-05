78f7a12f3c8c9f9e88703a3fb95ad8f1cfa5df1b
kwambzval <valentinevkkv@gmail.com>
2024-02-05 13:59:22 -0800

    task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class 'BaseGeometry'.
+"""
+
+
+class BaseGeometry:
+    """
+    This is the 'BaseGeometry' class.
+
+    This class doesn't contain any attributes or methods.
+    """
+    pass
