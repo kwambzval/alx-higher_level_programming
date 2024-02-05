c06eb5d3afbd1578217b44c00c2137cfe4a43a95
kwambzval <valentinevkkv@gmail.com>
2024-02-05 13:25:05 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is the 'my_module' module.
+
+This module demonstrates a simple Python script adhering to
+the provided requirements.
+"""
+
+
+class MyClass:
+    """
+    This is the 'MyClass' class.
+
+    This class demonstrates a simple Python class within
+    the 'my_module' module.
+    """
+
+    def my_function(self):
+        """
+        This is the 'my_function' method.
+
+        This method demonstrates a simple function within the 'MyClass' class.
+        """
+        return "Hello, World!"
