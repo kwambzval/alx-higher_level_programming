16b358ab345ac1d18b8db8991fa292b159eaf2b9
kwambzval <valentinevkkv@gmail.com>
2024-02-06 10:41:29 -0800

    task 0 & 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Module for reading a text file (UTF8) and printing it to stdout.
+
+This module contains a function, read_file, which reads a text file (UTF8)
+and prints it to stdout. The filename is passed as a parameter to the function.
+"""
+
+
 def read_file(filename=""):
     """
     Function that reads a text file (UTF8) and prints it to stdout
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+Module for writing a string to a text file (UTF8) and returning
+the number of characters written.
+
+This module contains a function, write_file, which writes a string
+to a text file (UTF8) and returns the number of characters written.
+The filename and the text to be written
+are passed as parameters to the function.
+"""
+
+
 def write_file(filename="", text=""):
     """
     Function that writes a string to a text file (UTF8) and returns
