89bd4284a23ed3baf97b9df9728795764dd7bf67
kwambzval <valentinevkkv@gmail.com>
2024-03-13 13:57:12 -0700

    task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- List all shows and all genres linked to each show
+SELECT tv_shows.title, genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN genres ON tv_show_genres.genre_id = genres.id
+ORDER BY tv_shows.title, genres.name;
+
