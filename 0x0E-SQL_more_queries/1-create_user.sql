e4a6687e6b2de5e2f269bd049b71a4dd1339e148
kwambzval <valentinevkkv@gmail.com>
2024-03-13 12:55:10 -0700

    task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create user user_0d_1 if it doesn't exist and grant all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
