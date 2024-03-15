1ad44f83ab9216eab4d2e52938eff0aee64c3235
kwambzval <valentinevkkv@gmail.com>
2024-03-15 10:16:40 -0700

    task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,20 @@
--- Script to convert database, table, and field to UTF8
-USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Check database character set and collation
+SELECT default_character_set_name, default_collation_name
+FROM information_schema.SCHEMATA
+WHERE schema_name = 'hbtn_0c_0';
+
+-- Check table character set and collation
+SELECT CCSA.character_set_name, CCSA.collation_name
+FROM information_schema.`TABLES` T,
+information_schema.`COLLATION_CHARACTER_SET_APPLICABILITY` CCSA
+WHERE CCSA.collation_name = T.table_collation
+AND T.table_schema = "hbtn_0c_0"
+AND T.table_name = "first_table";
+
+-- Check column character set and collation
+SELECT character_set_name, collation_name
+FROM information_schema.`COLUMNS` 
+WHERE table_schema = "hbtn_0c_0"
+AND table_name = "first_table"
+AND column_name = "name";
 
