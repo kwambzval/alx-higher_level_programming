74b031ff1d49390552127c4eec2235552df51aa4
kwambzval <valentinevkkv@gmail.com>
2024-04-29 09:06:22 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Fetches & displays response body for 200 status code (silent mode)
-curl -s -o /dev/null -w "%{http_code}\n"  "$@" | grep 200 && curl -s "$@"
+# This script sends a GET request to the URL passed as the first argument and displays the body of a 200 status code response
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
