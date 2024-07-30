49479003fac85f7b45914e868395f100821d30ea
kwambzval <valentinevkkv@gmail.com>
2024-07-30 10:17:37 -0700

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Script to print the title of a Star Wars movie by episode number
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
+
